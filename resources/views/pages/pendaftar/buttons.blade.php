a70432a722b0de7d32dfba04319cad8685f49586
yudhioktaviangule <yudhioktavian@icloud.com>
2021-03-23 16:31:23 +0800

    doner

--- resources/views/pages/pendaftar/buttons.blade.php
+++ resources/views/pages/pendaftar/buttons.blade.php
@@ -3,6 +3,7 @@
         <a href="#" class="btn btn-primary btn-sm"><i class="fas fa-edit"></i></a>
         <a href="#" class="btn btn-danger btn-sm"><i class="fas fa-times"></i></a>
     @else
-        <a href="{{route('pendaftar.show',['pendaftar'=>$decode->id])}}" class="btn btn-success btn-sm"><i class="fas fa-check"></i> Aktivasi</a>
+        <a href="{{route('pendaftar.show',['pendaftar'=>$decode->id])}}" class="btn btn-success btn-sm"><i class="fas fa-check"></i></a>
     @endif
+    <a href="{{route('pendaftar.show',['pendaftar'=>$decode->id])}}" class="btn btn-success btn-sm"><i class="fas fa-print"></i> Cetak</a>
 </div>
