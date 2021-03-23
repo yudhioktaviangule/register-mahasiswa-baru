55ba2b2d4b9086e0674c23578bccb55bd5970500
yudhioktaviangule <yudhioktavian@icloud.com>
2021-03-23 16:39:26 +0800

    doner

--- resources/views/pages/pendaftar/buttons.blade.php
+++ resources/views/pages/pendaftar/buttons.blade.php
@@ -1,7 +1,7 @@
 <div class="text-right">
     @if($decode->verifikasi==='aktif')
         <a href="#" class="btn btn-primary btn-sm"><i class="fas fa-edit"></i></a>
-        <a href="#" onclick="hapus(`{{route('pendaftar.delete',['pendaftar'=>$decode->id])}}`)" class="btn btn-danger btn-sm"><i class="fas fa-times"></i></a>
+        <a href="#" onclick="hapus(`{{route('pendaftar.destroy',['pendaftar'=>$decode->id])}}`)" class="btn btn-danger btn-sm"><i class="fas fa-times"></i></a>
     @else
         <a href="{{route('pendaftar.show',['pendaftar'=>$decode->id])}}" class="btn btn-success btn-sm"><i class="fas fa-check"></i></a>
     @endif
